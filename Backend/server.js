1d472adb4ca6eb4e1d27344bf3dd7535b3aaabde
kakashi812 <tavishgupta9@gmail.com>
2026-04-08 09:47:36 +0530

    add docker sprt

--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,35 @@
-import express from 'express'   
-import { createServer } from 'http'
-import { Server } from 'socket.io'
-import { YSocketIO } from 'y-socket.io/dist/server'
+import express from "express"
+import { createServer } from "http"
+import { Server } from "socket.io"
+import { YSocketIO } from "y-socket.io/dist/server"
+
 
 const app = express()
+app.use(express.static("public"))
+
+
 const httpServer = createServer(app)
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World' ,
-    success: true
-  })
-})
-
-app.get('/health', (req, res) => {
-  res.status(200).json({ message: 'Server is healthy' ,
-    success: true
-  })
-})
-
 const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST']
-  }
+    cors: {
+        origin: "*",
+        methods: [ "GET", "POST" ]
+    }
 })
+
 
 const ySocketIO = new YSocketIO(io)
 ySocketIO.initialize()
 
 
-httpServer.listen(3000, () => {
-  console.log('Server is listening on port 3000')
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        message: "ok",
+        success: true
+    })
 })
+
+
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})  
